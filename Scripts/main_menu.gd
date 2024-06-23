3002fd696a3b03e0056a52ce7eddc728e3aad875
Quinn Eakes <eakesq@yahoo.com>
2024-06-23 11:02:30 -0500

    eee pause stuff

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 
 # woah
 func _on_button_pressed() -> void: 
-	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+	get_tree().change_scene_to_file("res://Scenes/track.tscn")
 
 func _on_options_pressed() -> void:
 	pass # Replace with function body.
---
+++ Scripts/pause_menu.gd
@@ -0,0 +1,9 @@
+extends Control
+
+# code from: https://www.youtube.com/watch?v=OWtwYp2lIlQ
+
+func _ready() -> void:
+	hide()
+
+func _process(delta: float) -> void:
+	pass
