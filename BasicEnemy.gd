dc47d1d55d9deb57fedb066a2dd859ffa92eaf7b
Matthew Mahan <themaskedkid321@gmail.com>
2024-06-23 15:58:29 -0500

    pidgeons walking

--- BasicEnemy.gd
+++ BasicEnemy.gd
@@ -1,5 +1,7 @@
 extends PathFollow3D
 
+@onready var animation: AnimationPlayer = $AnimationPlayer
+
 var health:int = 20
 var speed:int = 1
 
@@ -11,6 +13,16 @@ func _physics_process(delta: float) -> void:
 	progress += speed * delta 
 	if progress_ratio >= 1.0:
 		queue_free()
+
+func _process(delta: float) -> void:
+	if fmod(self.rotation_degrees.y + 360, 360.0) >= 45.0 and fmod(self.rotation_degrees.y + 360, 360.0) < 135.0:
+		animation.play("walk_west")
+	elif fmod(self.rotation_degrees.y + 360, 360.0) >= 135.0 and fmod(self.rotation_degrees.y + 360, 360.0) < 225.0:
+		animation.play("walk_south")
+	elif fmod(self.rotation_degrees.y + 360, 360.0) >= 225.0 and fmod(self.rotation_degrees.y + 360, 360.0) <= 315.0:
+		animation.play("walk_east")
+	else:
+		animation.play("walk_north")
 
 func _take_damage(damage: int) -> void:
 	health -= damage
