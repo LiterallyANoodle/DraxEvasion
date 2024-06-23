5116bae6fbbe609153d1c4252dc539f43688f868
Matthew Mahan <themaskedkid321@gmail.com>
2024-06-23 12:58:22 -0500

    placements in track

--- Scripts/placement_point.gd
+++ Scripts/placement_point.gd
@@ -5,10 +5,10 @@ class_name PlacementPoint
 @onready var hover: MeshInstance3D = $HoverVisualizer
 
 func _ready() -> void:
-	hover.visible = false
-
-func _on_mouse_entered() -> void:
 	hover.visible = true
 
-func _on_mouse_exited() -> void:
+func _on_mouse_entered() -> void:
 	hover.visible = false
+
+func _on_mouse_exited() -> void:
+	hover.visible = true
