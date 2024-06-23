c389fc8ab66bdd96e0e2b10102837e19e49ead99
Matthew Mahan <themaskedkid321@gmail.com>
2024-06-23 13:16:49 -0500

    tower placement

--- Scripts/placement_point.gd
+++ Scripts/placement_point.gd
@@ -1,6 +1,8 @@
 extends Area3D
 class_name PlacementPoint
 
+const tower_scene = preload("res://base_tower.tscn")
+
 @onready var tower: BaseTower = null
 @onready var hover: MeshInstance3D = $HoverVisualizer
 
@@ -8,7 +10,19 @@ func _ready() -> void:
 	hover.visible = true
 
 func _on_mouse_entered() -> void:
-	hover.visible = false
+	if tower == null:
+		hover.visible = false
 
 func _on_mouse_exited() -> void:
-	hover.visible = true
+	if tower == null:
+		hover.visible = true
+
+func _on_input_event(camera: Node, event: InputEvent, position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	if (event is InputEventMouseButton):
+		if (event.button_index == 1 and event.pressed == true):
+			if tower == null:
+				tower = tower_scene.instantiate()
+				add_child(tower)
+			else:
+				tower.queue_free()
+		
