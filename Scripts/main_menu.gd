eced039869f34e552feb4921309cc6ff0653ea83
Quinn Eakes <eakesq@yahoo.com>
2024-06-23 01:25:18 -0500

    Got a Main Menu Together

---
+++ Scripts/main_menu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+# woah
+func _on_button_pressed() -> void: 
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+
+func _on_options_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_Quit_button_pressed() -> void:
+	get_tree().quit()
